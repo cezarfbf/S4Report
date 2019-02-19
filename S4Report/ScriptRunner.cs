5c2183920eabf6e108343a572f4e90a6155cce1d
cezarfbf <cezarfbf@live.com>
2019-02-19 12:03:27 -0300

    commit build

---
+++ S4Report/ScriptRunner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace S4Report
+{
+    class ScriptRunner
+    {
+        private readonly Timer timer;
+
+        public ScriptRunner()
+        {
+            timer = new Timer(10000) { AutoReset = true };
+            timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(@".\Output\output.txt", lines);
+        }
+    }
+}
